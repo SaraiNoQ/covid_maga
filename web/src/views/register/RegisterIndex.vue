0bd908047f288d5873e85b7365372df859ea1f00
SaraiNoQ <2994498974@qq.com>
2022-04-27 16:26:22 +0800

    feat: 新增表单验证和信息录入

--- web/src/views/register/RegisterIndex.vue
+++ web/src/views/register/RegisterIndex.vue
@@ -1,24 +1,29 @@
 <template>
 <div>
+  <alert-message :message="errorInfo" type="error" ref="alertRef"/>
+  <alert-message message="录 入 成 功！" type="success" ref="successRef"/>
   <h2 class="mt-4 text-3xl text-green-600 font-semibold">学 生 信 息 录 入</h2>
     <div class="rg-shadow mt-2 w-[75vw] md:w-[60vw] lg:w-[50vw] xl:w-[40vw] mx-auto bg-gray-200 pt-4 pr-12 pb-2">
-        <el-form :model="form" label-width="120px">
-          <el-form-item label="Name">
+        <el-form
+          :model="form"
+          label-width="120px"
+          :rules="rules">
+          <el-form-item label="Name" prop="name">
             <el-input v-model="form.name" />
           </el-form-item>
           
-          <el-form-item label="Gender">
+          <el-form-item label="Gender" prop="gender">
             <el-radio-group v-model="form.resource">
               <el-radio label="male" />
               <el-radio label="female" />
             </el-radio-group>
           </el-form-item>
 
-          <el-form-item label="Number">
+          <el-form-item label="Number" prop="number">
               <el-input v-model="form.number" />
           </el-form-item>
 
-          <el-form-item label="Major">
+          <el-form-item label="Major" prop="major">
               <el-input v-model="form.major" />
           </el-form-item>
 
@@ -46,7 +51,7 @@
           </el-form-item>
 
           <el-form-item>
-            <el-button type="primary" @click="onSubmit">Create</el-button>
+            <el-button type="primary" @click="onSubmit" :loadong="buttonLoading">Create</el-button>
             <el-button @click="onReset">Reset</el-button>
           </el-form-item>
         </el-form>
@@ -58,47 +63,102 @@
 import { defineComponent, reactive, ref } from 'vue'
 import { genFileId } from 'element-plus'
 import type { UploadInstance, UploadProps, UploadRawFile } from 'element-plus'
+import Axios from '../../plugins/axios'
 
+import AlertMessage from '../../components/AlertMessage.vue'
 
 export default defineComponent({
-    setup() {
-        const form = reactive({
-            name: '',
-            resource: 'male',
-            major: '',
-            number: ''
-        })
-        const onSubmit = () => {
-            console.log('submit')
-        }
-        const onReset = () => {
-            form.name = ''
-            form.resource = ''
-            form.major = ''
-            form.number = ''
-            form.resource = 'male'
-            upload_img.value!.clearFiles()
-        }
+  components: { AlertMessage },
+  setup() {
+    const form = reactive({
+      name: '',
+      resource: '',
+      major: '',
+      number: ''
+    })
 
-        const upload_img = ref<UploadInstance>()
-        const handleExceed: UploadProps['onExceed'] = (files) => {
-          upload_img.value!.clearFiles()
-          const file = files[0] as UploadRawFile
-          file.uid = genFileId()
-          upload_img.value!.handleStart(file)
-        }
-        const submitUpload = () => {
-          upload_img.value!.submit()
-        }
-        return {
-            onSubmit,
-            onReset,
-            handleExceed,
-            submitUpload,
-            form,
-            upload_img
-        }
-    },
+    // 表单输入验证
+    const validateNumber = (rule: any, value: any, callback: any) => {
+      if (value === '') {
+        callback(new Error('学号不能为空'))
+      } else if (value.length !== 10) {
+        callback(new Error('请输入正确的学号'))
+      } else {
+        callback()
+      }
+    }
+    const validateName = (rule: any, value: any, callback: any) => {
+      if (value.length < 2) {
+        callback(new Error('请输入正确的姓名'))
+      } else {
+        callback()
+      }
+    }
+    const rules = reactive({
+      name: [{ validator: validateName, trigger: 'blur' }],
+      gender: [{ required: true, trigger: 'blur' }],
+      number: [{ validator: validateNumber, trigger: 'blur' }],
+      major: [{ required: true, trigger: 'blur' }]
+    })
+
+    // 调用组件
+    const alertRef = ref<any>(null)
+    const successRef = ref<any>(null)
+    const errorInfo = ref<string>('录 入 失 败！')
+    const buttonLoading = ref<Boolean>(false)
+    const onSubmit = async () => {
+      buttonLoading.value = true
+      const formData = new FormData()
+      formData.append('student_name', form.name)
+      formData.append('student_number', form.number)
+      formData.append('student_gender', form.resource)
+      formData.append('student_major', form.major)
+      // if (upload_img.value) formData.append('student_image', upload_img.value)
+      const res = await Axios.post('/student/create', formData)
+      // @ts-ignore
+      if (res.success) {
+        await successRef.value.setDis()
+      } // @ts-ignore 
+      else if (res.error.data.code === '20102') {
+        errorInfo.value = '注 册 学 生 已 存 在！'
+        await alertRef.value.setDis()
+      }
+      buttonLoading.value = false
+    }
+
+    const onReset = () => {
+      form.name = ''
+      form.resource = ''
+      form.major = ''
+      form.number = ''
+      form.resource = 'male'
+      upload_img.value!.clearFiles()
+    }
+
+    const upload_img = ref<UploadInstance>()
+    const handleExceed: UploadProps['onExceed'] = (files) => {
+      upload_img.value!.clearFiles()
+      const file = files[0] as UploadRawFile
+      file.uid = genFileId()
+      upload_img.value!.handleStart(file)
+    }
+    const submitUpload = () => {
+      upload_img.value!.submit()
+    }
+    return {
+        onSubmit,
+        onReset,
+        handleExceed,
+        submitUpload,
+        form,
+        upload_img,
+        rules,
+        alertRef,
+        successRef,
+        errorInfo,
+        buttonLoading
+    }
+  },
 })
 </script>
 
