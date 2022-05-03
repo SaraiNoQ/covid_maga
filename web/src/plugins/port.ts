3e1160bcedab4ac901bcebbe860f49f4996950e7
SaraiNoQ <75473440+SaraiNoQ@users.noreply.github.com>
2022-05-03 13:15:53 +0800

    Update port.ts

--- web/src/plugins/port.ts
+++ web/src/plugins/port.ts
@@ -1,9 +1,4 @@
 export default {
-<<<<<<< Updated upstream
     // 上传云端后，改为自己的ip/images/
     imageUrlPrefix: 'http://localhost:5015/images/'
 }
-=======
-    imageUrlPrefix: 'http://82.157.232.71/images/'
-}
->>>>>>> Stashed changes
